3ca235f30d1a9527ed45b6c83a1f8df76012d2aa
Nikola <nikolavasovski@yahoo.com>
2022-09-04 21:05:45 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace TicTacToe_Vp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.playerXLabel = new System.Windows.Forms.Label();
+            this.playerOLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playAgain = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.draws = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 80);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(583, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 80);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(669, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 80);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(497, 173);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 80);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(583, 173);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 80);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(669, 173);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 80);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(497, 259);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 80);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(583, 259);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 80);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(669, 259);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 80);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(25, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 24);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MK";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.MK_Lang_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(83, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(49, 24);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "EN";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.EN_Lang_CheckedChanged);
+            // 
+            // playerXLabel
+            // 
+            this.playerXLabel.AutoSize = true;
+            this.playerXLabel.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.playerXLabel.Location = new System.Drawing.Point(25, 87);
+            this.playerXLabel.Name = "playerXLabel";
+            this.playerXLabel.Size = new System.Drawing.Size(216, 62);
+            this.playerXLabel.TabIndex = 11;
+            this.playerXLabel.Text = "Player X: ";
+            // 
+            // playerOLabel
+            // 
+            this.playerOLabel.AutoSize = true;
+            this.playerOLabel.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.playerOLabel.Location = new System.Drawing.Point(25, 169);
+            this.playerOLabel.Name = "playerOLabel";
+            this.playerOLabel.Size = new System.Drawing.Size(223, 62);
+            this.playerOLabel.TabIndex = 12;
+            this.playerOLabel.Text = "Player O: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(288, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 62);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(288, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 62);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "0";
+            // 
+            // playAgain
+            // 
+            this.playAgain.Location = new System.Drawing.Point(80, 341);
+            this.playAgain.Name = "playAgain";
+            this.playAgain.Size = new System.Drawing.Size(167, 69);
+            this.playAgain.TabIndex = 15;
+            this.playAgain.Text = "Play Again";
+            this.playAgain.UseVisualStyleBackColor = true;
+            this.playAgain.Click += new System.EventHandler(this.playAgain_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(288, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 62);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "0";
+            // 
+            // draws
+            // 
+            this.draws.AutoSize = true;
+            this.draws.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.draws.Location = new System.Drawing.Point(25, 243);
+            this.draws.Name = "draws";
+            this.draws.Size = new System.Drawing.Size(177, 62);
+            this.draws.TabIndex = 16;
+            this.draws.Text = "Draws: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.draws);
+            this.Controls.Add(this.playAgain);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.playerOLabel);
+            this.Controls.Add(this.playerXLabel);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.button_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label playerXLabel;
+        private System.Windows.Forms.Label playerOLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button playAgain;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label draws;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe_Vp
+{
+    public partial class Form1 : Form
+    {
+        private static Dictionary<String, String> mkTranslations = new Dictionary<String, String>() {
+            { "playerX", "Играч X: " },
+            { "playerO", "Играч О: " },
+            { "draws", "Нерешени: " },
+            { "alreadyClicked", "Ова поле е веќе кликнато." },
+            { "error", "Грешка!" },
+            { "victory", "Победа!" },
+            { "playAgain", "Играј повторно" }
+        };
+        private static Dictionary<String, String> enTranslations = new Dictionary<String, String>() {
+            { "playerX", "Player X: " },
+            { "playerO", "Player O: " },
+            { "draws", "Draws: " },
+            { "alreadyClicked", "Field already clicked by a player." },
+            { "error", "Error!" },
+            { "victory", "Victory!" },
+            { "playAgain", "Play Again" }
+
+        };
+
+        private Dictionary<String, Button> buttonsMap = new Dictionary<String, Button>();
+        private Boolean playerFlag = true;
+        private Boolean gameOver = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonsMap.Add("button1", button1);
+            buttonsMap.Add("button2", button2);
+            buttonsMap.Add("button3", button3);
+            buttonsMap.Add("button4", button4);
+            buttonsMap.Add("button5", button5);
+            buttonsMap.Add("button6", button6);
+            buttonsMap.Add("button7", button7);
+            buttonsMap.Add("button8", button8);
+            buttonsMap.Add("button9", button9);
+        }
+
+        private void MK_Lang_CheckedChanged(object sender, EventArgs e)
+        {
+            this.playerXLabel.Text = mkTranslations["playerX"];
+            this.playerOLabel.Text = mkTranslations["playerO"];
+            this.draws.Text = mkTranslations["draws"];
+            this.playAgain.Text = mkTranslations["playAgain"];
+        }
+
+        private void EN_Lang_CheckedChanged(object sender, EventArgs e)
+        {
+            this.playerXLabel.Text = enTranslations["playerX"];
+            this.playerOLabel.Text = enTranslations["playerO"];
+            this.draws.Text = enTranslations["draws"];
+            this.playAgain.Text = enTranslations["playAgain"];
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            string buttonName = (sender as Button).Name;
+            if (buttonsMap[buttonName].Text != "")
+            {
+                MessageBox.Show(radioButton1.Checked ? mkTranslations["alreadyClicked"] : enTranslations["alreadyClicked"],
+                                radioButton1.Checked ? mkTranslations["error"] : enTranslations["error"],
+                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            buttonsMap[buttonName].Text = playerFlag ? "X" : "O";
+
+            playerFlag = !playerFlag;
+            checkIfGameIsOver();
+        }
+
+        void checkIfGameIsOver()
+        {
+            List<String> btns = buttonsMap.Values.ToList().Select(d => d.Text).ToList();
+            List<int> winningIndexes = new List<int>();
+
+            for (int i = 0; i <= 6; i+= 3)
+            { 
+                if (btns[i] != "" && btns[i] == btns[i + 1] && btns[i] == btns[i + 2])
+                {
+                    getVictoryMessage(btns[i].ToString());
+                    winningIndexes.Add(i);
+                    winningIndexes.Add(i + 1);
+                    winningIndexes.Add(i + 2);
+                    gameOver = true;
+                    break;
+                }
+            }
+
+            for (int i = 0; i <= 2; i ++)
+            {
+                if (btns[i] != "" && btns[i] == btns[i + 3] && btns[i] == btns[i + 6])
+                {
+                    getVictoryMessage(btns[i].ToString());
+                    winningIndexes.Add(i);
+                    winningIndexes.Add(i + 3);
+                    winningIndexes.Add(i + 6);
+                    gameOver = true;
+                    break;
+                }
+            }
+
+            for (int i = 0; i <= 2; i += 2)
+            {
+                if (i == 0 && btns[i] != "" && btns[i] == btns[4] && btns[i] == btns[8])
+                {
+                    getVictoryMessage(btns[i].ToString());
+                    winningIndexes.Add(0);
+                    winningIndexes.Add(4);
+                    winningIndexes.Add(8);
+                    gameOver = true;
+                    break;
+                }
+                if (i == 2 && btns[i] != "" && btns[i] == btns[4] && btns[i] == btns[6])
+                {
+                    getVictoryMessage(btns[i].ToString());
+                    winningIndexes.Add(2);
+                    winningIndexes.Add(4);
+                    winningIndexes.Add(6);
+                    gameOver = true;
+                    break;
+                }
+            }
+
+            winningIndexes.ForEach(i => {
+                buttonsMap["button" + (i + 1)].BackColor = Color.Green;
+            });
+
+            if (gameOver)
+            {
+                if (playerFlag)
+                {
+                    this.label1.Text = (Int64.Parse(this.label1.Text) + 1).ToString();
+                }
+                else
+                {
+                    this.label2.Text = (Int64.Parse(this.label2.Text) + 1).ToString();
+                }
+            }
+
+            if (btns.TrueForAll(b => b != "") && !gameOver)
+            {
+                MessageBox.Show(radioButton1.Checked ? "Нерешено" : "Draw",
+                                radioButton1.Checked ? "Нерешено" : "Draw",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gameOver = true;
+                buttonsMap.Values.ToList().ForEach(b =>
+                {
+                    b.BackColor = Color.Yellow;
+                });
+                    return;
+            }
+
+        }
+
+        private void playAgain_Click(object sender, EventArgs e)
+        {
+            gameOver = false;
+            buttonsMap = new Dictionary<String, Button>();
+            buttonsMap.Add("button1", button1);
+            buttonsMap.Add("button2", button2);
+            buttonsMap.Add("button3", button3);
+            buttonsMap.Add("button4", button4);
+            buttonsMap.Add("button5", button5);
+            buttonsMap.Add("button6", button6);
+            buttonsMap.Add("button7", button7);
+            buttonsMap.Add("button8", button8);
+            buttonsMap.Add("button9", button9);
+
+            buttonsMap.Values.ToList().ForEach(b =>
+            {
+                b.Text = "";
+                b.BackColor = Color.White;
+                b.Click -= new System.EventHandler(this.button_Click);
+                b.Click += new System.EventHandler(this.button_Click);
+            });
+
+
+        }
+
+        private void getVictoryMessage(string player)
+        {
+            MessageBox.Show(radioButton1.Checked ? "Играчот " + player + " победи" : "Player " + player + " has won", 
+                            radioButton1.Checked ? mkTranslations["victory"] : enTranslations["victory"],
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
